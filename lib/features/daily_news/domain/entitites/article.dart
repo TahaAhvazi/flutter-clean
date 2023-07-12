5ceac25c5917175be9bf07d56ee6eb7d44bfbe7b
Taha Ahvazi <tahax.dev@gmail.com>
2023-07-12 13:47:30 +0330

    Define the entites in our project

---
+++ lib/features/daily_news/domain/entitites/article.dart
@@ -0,0 +1,34 @@
+import 'package:equatable/equatable.dart';
+
+class ArticleEntity extends Equatable {
+  final int? id;
+  final String? author,
+      title,
+      description,
+      url,
+      urlToImage,
+      publishedAt,
+      content;
+
+  const ArticleEntity(
+    this.id,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  );
+  @override
+  List<Object?> get props => [
+        id,
+        author,
+        title,
+        description,
+        url,
+        urlToImage,
+        publishedAt,
+        content,
+      ];
+}
